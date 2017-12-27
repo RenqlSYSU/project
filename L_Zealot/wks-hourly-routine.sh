25dd8506501bd77bba3bd6d29698c5053baee002
ys17-19 <ys17-19@localhost.localdomain>
2017-12-27 13:42:12 +0800

    delete clm-month

--- L_Zealot/wks-hourly-routine.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/yangsong3/www/html/wordpress/wp-content/figs
-php get_fig.php
--- L_Zealot/wks-minutely-routine.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Get raw economy data
-sh /home/yangsong3/L_Zealot/workspace/economy/get_raw_data.sh  >/dev/null 2>&1

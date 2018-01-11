c10dd23f72575803e2bd13b2ab2368089e43806e
ys17-19 <ys17-19@localhost.localdomain>
2018-01-11 18:08:59 +0800

    delete

--- sync-delete-hpc4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#HPC4 East Campus
-# rsync -vzrtuopg  /home/ys17-19/renql/project/ yangsong3@hpc4.sysu.edu.cn:/users/yangsong3/renql/project/  >> ~/cron/sys_report/$LID-project-report
- rsync -vzrtuopg -progress --delete  yangsong3@hpc4.sysu.edu.cn:/users/yangsong3/renql/project/ /home/ys17-19/renql/project/  >> ~/cron/sys_report/$LID-project-report

294357b8ea09fef723ddce643c486292c89b2aee
RenqlSYSU <1797299307@qq.com>
2020-11-01 04:02:07 +0800

    201101

--- TP_NUDG/z_tendency-20191022/1911-iterative_monthly_sigm.f90
+++ TP_NUDG/z_tendency-20191022/1911-iterative_monthly_sigm.f90
@@ -13,7 +13,7 @@ program iteration
     !integer, parameter :: nlat = 54 , nlon = 240 , ilog  = 10 , ifile = 12
     !integer, parameter :: pr = 8 , ncase = 3 , nvar = 3 ,nvar2 = 2, ntime = 1, nlev = 19 !NCEP1
     !integer, parameter :: nlat = 31 , nlon = 144 , ilog  = 10 , ifile = 12
-    integer, parameter :: pr = 8 , ncase = 3 , nvar = 4 ,nvar2 = 2, ntime = 4, nlev = 19
+    integer, parameter :: pr = 8 , ncase = 3 , nvar = 3 ,nvar2 = 2, ntime = 2, nlev = 19
     integer, parameter :: nlat = 85 , nlon = 288 , ilog  = 10 , ifile = 12
     integer :: nc, nv, nt, nz, ny, nx, iter, irec 
     
@@ -41,7 +41,7 @@ program iteration
     
 !    print*, "please input the number of case,1 or 2 or 3"
 !    read(*,*) nc
-    nc = 2
+    nc = 6
     if(nc.eq.1) then 
         filename = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/mdata/CTRL-Clim_4f6c_month.dat"
         fileout  = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/mdata/CTRL-Clim_dzdt_month.dat"     
@@ -69,13 +69,13 @@ program iteration
         fileout  = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/mdata/NUDG-Clim_dzdt_month.dat"     
         logname = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/f90_iter_infor-NUDG.txt"
     else 
-        filename = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/mdata/NG58-Clim_4f6c_month.dat"
-        fileout  = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/mdata/NG58-Clim_dzdt_month.dat"     
-        logname = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/f90_iter_infor-NG58.txt"
+        filename = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/mdata/NGTR-Clim_4f6c_month.dat"
+        fileout  = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/mdata/NGTR-Clim_dzdt_month.dat"     
+        logname = "/home/ys17-19/renql/project/TP_NUDG/z_tendency-20191022/f90_iter_infor-NGTR.txt"
     end if
     
-    !var_name = (/"f_Qd   ","f_Qeddy","A      "/) !,"f_Qd_t "
-    var_name  = (/"f_Qeddh","f_Qeddl","Ah     ","Al     "/)
+    var_name = (/"f_Qd   ","f_Qeddy","A      "/) !,"f_Qd_t "
+    !var_name  = (/"f_Qeddh","f_Qeddl","Ah     ","Al     "/)
     open(unit=ilog,file=logname,form='formatted',status='replace')
     write(ilog,*) "relaxing factor is ", rf
     write(ilog,*) "critical value is ", critical

7602122a5bbce7c6e882d064fb07cfce6cfb0723
RenqlSYSU <1797299307@qq.com>
2021-12-23 04:02:08 +0800

    211223

---
+++ 2020MiddleEastJet/2112-draw_heatmap_corr_project_dzdt.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import matplotlib 
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+
+path = "/home/ys17-19/renql/project/2020MiddleEastJet/"
+area = ["ME1","ME2","EA1","EA2","NA1","NA2"]
+forc = ["Qd","lQte","lFte","hQte","hFte","Tadv","vort"]
+corr = np.loadtxt(path+"project_dzdt_corr.txt")
+prob = np.loadtxt(path+"project_dzdt_prob.txt")
+print(corr)
+print(prob)
+
+def draw_relation(corr,prob,labelx,labely,figdir):
+    midfont=10
+    smfont=16
+    cnlevels = [-0.99,-0.95,-0.90,0.90,0.95,0.99]
+    listcolor = ["dodgerblue","deepskyblue","powderblue","white","gold","darkorange","red"]
+    fcolors = colors.ListedColormap(listcolor)
+    norm = colors.BoundaryNorm(boundaries=cnlevels, ncolors=fcolors.N,extend='both')
+    
+    fig = plt.figure(figsize=(9,9),dpi=200)
+    axe = fig.add_axes([0.05, 0.05, 0.9, 0.6])
+    #axe.set_title(title,fontsize=midfont) 
+            
+    im = axe.imshow(prob, cmap=fcolors, norm=norm)
+    axe.set_xticks(np.arange(len(labelx)))
+    axe.set_yticks(np.arange(len(labely)))
+    axe.set_xticklabels(labelx,fontsize=smfont)
+    axe.set_yticklabels(labely,fontsize=smfont)
+    axe.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+    axe.plot([1.5,1.5],[-0.5,(len(labely)-0.5)],color="k")
+    axe.plot([3.5,3.5],[-0.5,(len(labely)-0.5)],color="k")
+
+    #axe.spines[:].set_visible(False)
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(labely)):
+        for j in range(len(labelx)):
+            text = axe.text(j,i,"%.2f"%corr[i,j], ha="center", va="center", color="k",fontsize=smfont)
+
+    #position = fig.add_axes([0.92, 0.2, 0.01, 0.6]) #left, bottom, width, height
+    #cb = plt.colorbar(im, cax=position ,orientation='vertical')#, shrink=.9)
+    plt.colorbar(im, ax=axe, shrink=.8)
+    #fig.tight_layout(rect=(0,bmlo,1,1)) #w_pad=0.5,h_pad=0.001) #,
+    #fig.savefig(figdir)
+    fig.savefig(figdir+".png", bbox_inches='tight',pad_inches=0.08)
+
+prob = 1-prob
+prob = np.where(corr<0,-prob,prob)
+draw_relation(corr,prob,area,forc,path+"fig/eof_corr")
+
---
+++ 2020MiddleEastJet/run_eof.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ncl 2002-calc_eof_6kinds.ncl
+ncl 2002-draw_eof_regression_horizontal.ncl
+ncl 2002-draw_eof_regression_vertical.ncl
+

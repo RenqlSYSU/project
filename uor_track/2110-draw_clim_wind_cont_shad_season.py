78b3a443682a53780bc9268804536f2db07e8bb3
RenqlSYSU <1797299307@qq.com>
2022-11-25 04:01:17 +0800

    221125

--- uor_track/2110-draw_clim_wind_cont_shad_season.py
+++ uor_track/2110-draw_clim_wind_cont_shad_season.py
@@ -29,8 +29,8 @@ font = {'family': 'sans-serif',
         'color':  'black', 
         }
 
-lonl=0  #0  #
-lonr=150#360#
+lonl=45  #0  #
+lonr=135#360#
 lats=15 #0  #
 latn=70 #90 #
 lat_sp = 20
@@ -49,6 +49,7 @@ def main_run():
     #read_draw_seasonal_4x3('w',1,[-0.08,0.01],'omega','Pa/s')
     #read_draw_seasonal_4x3('q',1000,[0,1],'q','g/kg')
     read_draw_seasonal_4x3('z','zonal',[-105,15],'ano_z','gpm')
+    #read_draw_seasonal_4x3('u',1,[0,5],'U','km/s')
     #read_draw_seasonal_4x3('Q1',1,[-0.07,0.01],'Q1','W/kg')
 
 def read_draw_seasonal_4x3(varname,scale,cnlev,label,unit):
@@ -83,12 +84,12 @@ def read_draw_seasonal_4x3(varname,scale,cnlev,label,unit):
         ds = xr.open_dataset('%s/ERA5_mon_%s_1979-2020.nc'%(path,varname))
         da = ds[varname].sel(level=lev[nl],latitude=ilat,longitude=ilon,method="nearest").load()
         if scale=='zonal':
-            da = ds[varname].sel(level=lev[nl]).load()
-            var = da.groupby(da.time.dt.month).mean('time')/9.8
-            var.data = np.moveaxis(np.moveaxis(var.data,2,0)-var.data.mean(2), 0, 2)
-            var = var.sel(latitude=ilat,longitude=ilon,method="nearest").data
-            #var = da.groupby(da.time.dt.month).mean('time').data/9.8
-            #var = np.moveaxis(np.moveaxis(var,2,0)-var.mean(2), 0, 2)
+            #da = ds[varname].sel(level=lev[nl]).load()
+            #var = da.groupby(da.time.dt.month).mean('time')/9.8
+            #var.data = np.moveaxis(np.moveaxis(var.data,2,0)-var.data.mean(2), 0, 2)
+            #var = var.sel(latitude=ilat,longitude=ilon,method="nearest").data
+            var = da.groupby(da.time.dt.month).mean('time').data/9.8
+            var = np.moveaxis(np.moveaxis(var,2,0)-var.mean(2), 0, 2)
         else:
             var = da.groupby(da.time.dt.month).mean('time').data*scale
         

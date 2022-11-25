4bab2a60847718769e59dec930e6ec065e677655
RenqlSYSU <1797299307@qq.com>
2022-11-26 04:01:12 +0800

    221126

--- uor_track/2110-draw_clim_wind_cont_shad_season.py
+++ uor_track/2110-draw_clim_wind_cont_shad_season.py
@@ -48,8 +48,8 @@ def main_run():
     #read_draw_seasonal_4x3('vo',100000,[-3.5,0.5],'vor','s-1')
     #read_draw_seasonal_4x3('w',1,[-0.08,0.01],'omega','Pa/s')
     #read_draw_seasonal_4x3('q',1000,[0,1],'q','g/kg')
-    read_draw_seasonal_4x3('z','zonal',[-105,15],'ano_z','gpm')
-    #read_draw_seasonal_4x3('u',1,[0,5],'U','km/s')
+    #read_draw_seasonal_4x3('z','zonal',[-105,15],'ano_z','gpm')
+    read_draw_seasonal_4x3('u',1,[-40,5],'U','km/s')
     #read_draw_seasonal_4x3('Q1',1,[-0.07,0.01],'Q1','W/kg')
 
 def read_draw_seasonal_4x3(varname,scale,cnlev,label,unit):
--- uor_track/2207-draw_stat_seasonal_shad_contour.py
+++ uor_track/2207-draw_stat_seasonal_shad_contour.py
@@ -26,31 +26,42 @@ font = {'family': 'sans-serif',
 lev = [850,500,250]
 prefix = "ff"
 #suffix = 'local'#'remote'# 
-suffix = '_6outside'#,''#'_6local'#,'_total']
+suffix = 'matched local' 
+#suffix = '_6local'#,'_total']'_6outside'#,''#
 titls= ['DJF','MAM','JJA','SON']
 numod= [chr(i) for i in range(97,115)]
 figdir = "/home/ys17-23/Extension2/renql/project/uor_track/fig"
 path = '/home/ys17-23/Extension2/renql/ERA5-1HR-lev/statistic'
 uwndpath = '/home/ys17-23/Extension2/renql/ERA5_mon/ERA5_mon_u_1979-2020.nc'
-if suffix in ['_6local','local','remote']:
+if suffix in ['matched local']:
     lonl=50  #0  #
-    lonr=150#360#
+    lonr=120#360#
+    lats=15 #20 #
+    latn=55 #90 #
+    suf1=suffix
+    bmlo = 0.22 #0.25 #
+if suffix in ['_6local','local','remote']:
+    lonl=55  #0  #
+    lonr=155#360#
     lats=15 #20 #
     latn=55 #90 #
     suf1='local'
+    bmlo = 0.37 #0.25 #
 if suffix == '_6outside':
     lonl=20  #0  #
     lonr=140#360#
     lats=15 #20 #
     latn=60 #90 #
     suf1='remote'
+    bmlo = 0.4 #0.25 #
 if suffix == '':
     lonl=0 
     lonr=150
     lats=15 
     latn=70 
     suf1='total'
-lat_sp = 20
+    bmlo = 0.4 #0.25 #
+lat_sp = 15
 lon_sp = 30 #60 #
 
 files = '%s/ff_250_1980-2020_stat.nc'%path
@@ -73,6 +84,7 @@ del ds
 gc.collect()
 
 def main_run():
+    '''
     # 6local & 6outside
     cnlev1 = np.hstack((np.arange(1,4,1),np.arange(7,20,3)))
     cnlev2 = np.hstack((np.arange(1,8.5,2.5),np.arange(13.5,50,5)))
@@ -84,11 +96,12 @@ def main_run():
     # local match remote or local
     cnlev1 = np.hstack((np.arange(0.5, 2 ,0.5),np.arange( 2 ,20 , 1))) # genesis & lysis
     cnlev2 = np.hstack((np.arange(0.5,3.5, 1 ),np.arange(3.5,50 , 2))) # track
-    draw_shad_cont_seasonal_4x3(suffix,'msp',np.arange(15,85,5),'Speed',
-            'gden',cnlev1,'Genesis','lden',cnlev1,'Lysis',False,[2,60])
-    draw_shad_cont_seasonal_4x3(suffix,'mstr',np.arange(0,14,1),'Intensity',
-            'tden',cnlev2,'Track','lden',cnlev1,'U200',True,[3.5,60])
-    '''
+    draw_match_shad_cont_seasonal_4x3('gden',np.arange(0.5,7,0.5),'Genesis',
+            'gden',cnlev1,'Genesis','gden',cnlev1,'Gensis')
+    #draw_shad_cont_seasonal_4x3(suffix,'msp',np.arange(15,85,5),'Speed',
+    #        'gden',cnlev1,'Genesis','lden',cnlev1,'Lysis',False,[2,60])
+    #draw_shad_cont_seasonal_4x3(suffix,'mstr',np.arange(0,14,1),'Intensity',
+    #        'tden',cnlev2,'Track','lden',cnlev1,'U200',True,[3.5,60])
 
     '''
     # total cyclones
@@ -105,9 +118,9 @@ def draw_shad_cont_seasonal_4x3(suffix,varname,cnlev,label,
     
     nrow = 4 #6 #
     ncol = 3 #2 #
-    bmlo = 0.4 #0.25 #
+    #bmlo = 0.37 #0.25 #
 
-    fig = plt.figure(figsize=(12,12),dpi=150)
+    fig = plt.figure(figsize=(12,12),dpi=300)
     ax = fig.subplots(nrow, ncol, subplot_kw=dict(
         projection=ccrs.PlateCarree(central_longitude=180.0))) #sharex=True, sharey=True
     
@@ -121,15 +134,26 @@ def draw_shad_cont_seasonal_4x3(suffix,varname,cnlev,label,
         ncolors=ncmap.N,extend='both')
     
     for nl in range(0,len(lev),1):
-        #files = '/home/ys17-23/Extension2/renql/project/uor_track/mdata/statistic/ff_match_%dlocal%s_6dist.nc'%(lev[nl],suffix)
-        files = '%s/%s_%d_1980-2020%s_stat.nc'%(path,prefix,lev[nl],suffix)
-        f = xr.open_dataset(files)
-        print("")
-        print(files)
-        var = read_stat(f,varname,lev[nl])
-        var1 = read_stat(f,varname1,lev[nl])
-        var2 = read_stat(f,varname2,lev[nl])
-    
+        if suffix == 'matched local':
+            files = '%s/%s_%d_1980-2020_6local_stat.nc'%(path,prefix,lev[nl])
+            f = xr.open_dataset(files)
+            var = read_stat(f,varname,lev[nl])
+
+            f = xr.open_dataset('/home/ys17-23/Extension2/renql/project/uor_track/mdata/statistic/ff_match_%dlocalremote_6dist.nc'%(lev[nl]))
+            var1 = read_stat(f,varname1,lev[nl])
+
+            f = xr.open_dataset('/home/ys17-23/Extension2/renql/project/uor_track/mdata/statistic/ff_match_%dlocallocal_6dist.nc'%(lev[nl]))
+            var2 = read_stat(f,varname2,lev[nl])
+        else:
+            #files = '/home/ys17-23/Extension2/renql/project/uor_track/mdata/statistic/ff_match_%dlocal%s_6dist.nc'%(lev[nl],suffix)
+            files = '%s/%s_%d_1980-2020%s_stat.nc'%(path,prefix,lev[nl],suffix)
+            f = xr.open_dataset(files)
+            print("")
+            print(files)
+            var = read_stat(f,varname,lev[nl])
+            var1 = read_stat(f,varname1,lev[nl])
+            var2 = read_stat(f,varname2,lev[nl])
+        
         for nm in range(0,nrow,1):
             if nm == 0:
                 shad = np.nanmean(np.array([var[0,:,:],var[1,:,:],var[11,:,:]]),axis=0)
@@ -268,6 +292,80 @@ def read_stat(f,varname,nl):
         np.nanmin(var.data),np.nanmax(var.data)))
     return var.data
     
+def draw_match_shad_cont_seasonal_4x3(varname,cnlev,label,
+    varname1,cnlev1,label1,varname2,cnlev2,label2):
+    
+    nrow = 4 #6 #
+    ncol = 3 #2 #
+
+    fig = plt.figure(figsize=(12,12),dpi=300)
+    ax = fig.subplots(nrow, ncol, subplot_kw=dict(
+        projection=ccrs.PlateCarree(central_longitude=180.0))) #sharex=True, sharey=True
+    
+    colr = cmaps.topo_15lev(range(0,16,1))[::-1]
+    print(colr)
+    colr[8,:] = colors.to_rgba('y')
+    #colr[8:13,:] = colr[9:14,:]
+    colr[7,:] = colors.to_rgba('c')
+    ncmap = colors.ListedColormap(colr[0:-1,:])
+    norm = colors.BoundaryNorm(boundaries=cnlev,
+        ncolors=ncmap.N,extend='both')
+    
+    for nl in range(0,len(lev),1):
+        files = '%s/%s_%d_1980-2020_6local_stat.nc'%(path,prefix,lev[nl])
+        f = xr.open_dataset(files)
+        var = read_stat(f,varname,lev[nl])
+
+        f = xr.open_dataset('/home/ys17-23/Extension2/renql/project/uor_track/mdata/statistic/ff_match_%dlocalremote_6dist.nc'%(lev[nl]))
+        var1 = read_stat(f,varname1,lev[nl])
+
+        f = xr.open_dataset('/home/ys17-23/Extension2/renql/project/uor_track/mdata/statistic/ff_match_%dlocallocal_6dist.nc'%(lev[nl]))
+        var2 = read_stat(f,varname2,lev[nl])
+        
+        for nm in range(0,nrow,1):
+            if nm == 0:
+                shad = np.nanmean(np.array([var[0,:,:],var[1,:,:],var[11,:,:]]),axis=0)
+                cont1 = np.nanmean(np.array([var1[0,:,:],var1[1,:,:],var1[11,:,:]]),axis=0)
+                cont2 = np.nanmean(np.array([var2[0,:,:],var2[1,:,:],var2[11,:,:]]),axis=0)
+                uwnd1 = np.nanmean(np.array([uwnd[0,:,:],uwnd[1,:,:],uwnd[11,:,:]]),axis=0)
+            else:
+                shad = np.nanmean(var[(3*nm-1):(3*nm+2),:,:],axis=0)
+                cont1 = np.nanmean(var1[(3*nm-1):(3*nm+2),:,:],axis=0)
+                cont2 = np.nanmean(var2[(3*nm-1):(3*nm+2),:,:],axis=0)
+                uwnd1 = np.nanmean(uwnd[(3*nm-1):(3*nm+2),:,:],axis=0)
+            axe = ax[nm][nl]
+            axe.add_feature(cfeat.GSHHSFeature(levels=[1,2],
+                edgecolor='k'), linewidth=0.8, zorder=1)
+            axe.set_title("(%s) %dhPa %s %s"%(numod[3*nm+nl],lev[nl],titls[nm],
+                suf1),fontsize=title_font,fontdict=font)
+
+            cont = axe.contourf(ilon, ilat, shad, cnlev, 
+                 transform=ccrs.PlateCarree(),cmap=ncmap,extend='both',norm=norm)
+            topo = axe.contour(ilon, ilat, phis, [1500,3000,4500], linestyles='solid', 
+                 transform=ccrs.PlateCarree(),colors='black',linewidths=1.5)
+
+            line1 = axe.contour(ilon, ilat, cont1, cnlev1, linestyles='solid',
+                 transform=ccrs.PlateCarree(),colors='b',linewidths=2.5)
+            
+            line2 = axe.contour(ilon, ilat, cont2, cnlev2, linestyles='solid', 
+                 transform=ccrs.PlateCarree(),colors='r',linewidths=2.5)
+            
+            if nl == 0:
+                axe.set_yticks(np.arange(lats,latn,lat_sp), crs=ccrs.PlateCarree())
+                axe.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
+            if nm == (nrow-1):
+                axe.set_xticks(np.arange(lonl,lonr,lon_sp), crs=ccrs.PlateCarree())
+                axe.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
+
+    position = fig.add_axes([0.45, bmlo-0.005, 0.45, 0.01]) #left, bottom, width, height
+    cb = plt.colorbar(cont, cax=position ,orientation='horizontal')#, shrink=.9)
+    plt.figtext(0.35,bmlo-0.005, label,fontsize=title_font,
+        horizontalalignment='left',verticalalignment='bottom')
+
+    plt.tight_layout(w_pad=0.5,rect=(0,bmlo,1,1))
+    plt.savefig('%s/stat_seasonal%s_%s.png'%(figdir,suffix,varname), 
+        bbox_inches='tight',pad_inches=0.01)
+
 if __name__=='__main__':
     main_run()
 
